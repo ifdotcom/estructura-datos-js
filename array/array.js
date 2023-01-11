37779aa62c8b09d9ba0b6c3c2b3cd85ab40f25e5
Fernanda Vidal <ifervb@gmail.com>
2023-01-10 18:01:29 -0600

    ADD: delete in array

--- array/array.js
+++ array/array.js
@@ -5,15 +5,37 @@ class myArray {
     this.length = 0;
     this.data = {};
   }
+  //  retorna el array
   get(index) {
     return this.data[index];
   }
+  //   agrega elementos al array
   push(item) {
     this.data[this.length] = item;
     this.length++;
     return this.data;
   }
-}
+  //   elimina el ultimo elemento
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
 
+  //   elimina un elemento random
+  delete(index) {
+    const item = this.data[index];
+    this.shiftIndex(index);
+    return item;
+  }
+  shiftIndex(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
 
 const arr = new myArray();
